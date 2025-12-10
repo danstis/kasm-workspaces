90c76c8d692b7d057b34b81612d1354068eb4d6e
Dan Anstis <dan@bsod.co.nz>
2025-12-11 08:48:19 +1000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/danstis/kasm-workspaces/issues',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-workspaces/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
